254e28c3597711091620157f0f6f16836657e8fb
rajivkr8207 <rajiv.kr8207@gmail.com>
2026-08-15 10:24:49 +0530

    feat: initialize backend project with Express dependencies and security middleware configuration

--- backend/src/app.middleware.js
+++ backend/src/app.middleware.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import morgan from 'morgan';
-import Config from './config/config.js';
 import helmet from 'helmet';
 import compression from 'compression';
+import Config from './config/Config.js';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
 
 const corsOption = {
   origin: Config.frontend_url,
@@ -13,13 +15,23 @@ const corsOption = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 100,
+  message: 'Too many requests from this IP, please try again in 15 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 export const Middleware = (app) => {
   app.use(express.json({ limit: '16kb' }));
   app.use(express.urlencoded({ extended: true, limit: '16kb' }));
   app.use(cookieParser());
   app.use(cors(corsOption));
-  app.use(morgan('dev'));
+  app.use(morgan(Config.env === 'production' ? 'combined' : 'dev'));
   app.use(helmet());
   app.use(compression());
   app.use(express.static('public/dist'));
+  app.use(hpp());
+  app.use('/api', limiter);
 };
